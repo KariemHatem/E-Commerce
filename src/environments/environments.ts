5bedce62e34b49e4c43888ffc7158f9e5b3e58ae
Kariem Hatem <kariemhatem2413@gamil.com>
2025-11-30 17:22:19 +0200

    change stripe url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'https://ecommerce.routemisr.com',
-  ecommerceUrl: 'http://localhost:4200/',
+  ecommerceUrl: 'https://e-commerce-ten-wheat-71.vercel.app/',
 };
