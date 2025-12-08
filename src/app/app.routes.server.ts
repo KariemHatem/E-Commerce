c7f2f375227a268fd7452a1905a1cbb60c5df3d1
Kariem Hatem <kariemhatem2413@gamil.com>
2025-12-08 13:46:26 +0200

    Fixing Issue of Server Routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,10 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'productsdetails/:id', renderMode: RenderMode.Server },
+  { path: 'address/:cartId', renderMode: RenderMode.Server },
   {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Prerender,
   },
 ];
