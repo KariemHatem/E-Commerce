a97544bd2c20617fc90498824df1af6fbdc7f97c
Kariem Hatem <kariemhatem2413@gamil.com>
2025-12-08 13:19:06 +0200

    solving issue in render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,6 +5,6 @@ export const serverRoutes: ServerRoute[] = [
   { path: 'address/:cartId', renderMode: RenderMode.Server },
   {
     path: '**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Server,
   },
 ];
