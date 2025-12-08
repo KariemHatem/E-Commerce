56f2f515d3b9575fcf969a3b814aad2d89583eb3
Kariem Hatem <kariemhatem2413@gamil.com>
2025-12-08 13:36:53 +0200

    Added vercel.json with a global rewrite rule to ensure all non-static

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'https://ecommerce.routemisr.com',
-  ecommerceUrl: 'https://e-commerce-ten-wheat-71.vercel.app/',
+  ecommerceUrl: 'https://e-commerce-nu-brown-59.vercel.app/',
 };
