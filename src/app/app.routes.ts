38de6872acc8a5818599c35eb3a05c8db8864634
Kariem Hatem <kariemhatem2413@gamil.com>
2025-12-08 14:12:52 +0200

    Adding Lazy Loading on components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { Products } from './features/pages/products/products';
 import { Cart } from './features/pages/cart/cart';
-import { Categories } from './features/pages/categories/categories';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
 import { NotFound } from './features/layout/not-found/not-found';
@@ -13,7 +11,13 @@ import { Address } from './features/pages/address/address';
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home, title: 'Home', canActivate: [authGuard] },
-  { path: 'products', component: Products, title: 'Products', canActivate: [authGuard] },
+  // Make Products Lazy Loading
+  {
+    path: 'products',
+    loadComponent: () => import('./features/pages/products/products').then((m) => m.Products),
+    title: 'Products',
+    canActivate: [authGuard],
+  },
   {
     path: 'productsdetails/:id',
     component: ProudctDetails,
@@ -21,7 +25,13 @@ export const routes: Routes = [
     canActivate: [authGuard],
   },
   { path: 'cart', component: Cart, title: 'Shopping Cart', canActivate: [authGuard] },
-  { path: 'categories', component: Categories, title: 'Categories', canActivate: [authGuard] },
+  // Make Categories Lazy Loading
+  {
+    path: 'categories',
+    loadComponent: () => import('./features/pages/categories/categories').then((m) => m.Categories),
+    title: 'Categories',
+    canActivate: [authGuard],
+  },
   { path: 'address/:cartId', component: Address, title: 'Address', canActivate: [authGuard] },
   { path: 'register', component: Register, title: 'Register' },
   { path: 'login', component: Login, title: 'Login' },
